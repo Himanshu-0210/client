404afc32447bad03a6fe0a4b3d4588ab15afea45
Himanshu-0210 <himanshuu288@gmail.com>
2025-08-19 23:03:55 +0530

    abc

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API =axios.create({
-    baseURL:'https://server-saf3.onrender.com/api',
+    baseURL:'https://coursebookingby-himanshu.netlify.app/api',
     withCredentials:true,
 })
 
